c375294f4b9af26ddcd47ff987ce83c957f23256
Riad Younes <riad.younes@hotmail.com>
2023-09-11 13:24:29 -0300

    first commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,26 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { globalStyles } from "@/styles/global";
+import type { AppProps } from "next/app";
+import { Roboto } from "next/font/google";
+import logoImg from "../assets/logo.svg";
+import { Container, Header } from "@/styles/pages/app";
+import Image from "next/image";
+
+const roboto = Roboto({
+  weight: ["400", "700"],
+  style: ["normal"],
+  subsets: ["latin"],
+  display: "swap",
+});
+
+globalStyles();
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Container className={roboto.className}>
+      <Header>
+        <Image src={logoImg} alt="" />
+      </Header>
+      <Component {...pageProps} />
+    </Container>
+  );
 }
